041b086e441659b55b323ff9cff95567c286e288
!gitUserName! <!gitUserEmail!>
2026-09-19 12:42:11 +0800

    加版本信息菜单与日期化版本号：①【查找】菜单新增【版本信息】项(显示版本号+编译时间),便于现场追溯;②AssemblyFileVersion/InformationalVersion 改为构建日期2026.09.19;③对应打标签v2026.09.19便于按时间检索版本

--- WindowsFormsApplication1/Properties/AssemblyInfo.cs
+++ WindowsFormsApplication1/Properties/AssemblyInfo.cs
@@ -33,4 +33,5 @@ using System.Runtime.InteropServices;
 // 方法是按如下所示使用“*”:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("2026.9.19.0")]
+[assembly: AssemblyInformationalVersion("2026.09.19")]
