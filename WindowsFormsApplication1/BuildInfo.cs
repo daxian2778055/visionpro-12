3e5a789d1dc65af14c55f95e6508c27056c56206
!gitUserName! <!gitUserEmail!>
2026-09-23 17:32:10 +0800

    版本号死值: 2026.09.23+ce86286（对应上一提交"第25轮朋友复审修正 R8 timer17 相位机节奏"的代码版本,推送后固定不变; 日期戳label148=YA0923与本次推送同日,无需更新）

--- WindowsFormsApplication1/BuildInfo.cs
+++ WindowsFormsApplication1/BuildInfo.cs
@@ -5,7 +5,7 @@ namespace WindowsFormsApplication1
 {
     internal static class BuildInfo
     {
-        public const string Version = "2026.09.23+9c7a61c";
-        public const string BuildTime = "2026-09-23 17:15:46";
+        public const string Version = "2026.09.23+ce86286";
+        public const string BuildTime = "2026-09-23 17:31:30";
     }
 }
